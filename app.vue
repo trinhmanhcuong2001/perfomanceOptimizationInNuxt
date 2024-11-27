b59198a2119a90ed188efec90f4f475039ca4797
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-27 17:01:32 +0700

    b3

--- app.vue
+++ app.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
+        <Suspense>
+            <template #default>
+                <component :is="Welcome" />
+            </template>
+            <template #fallback>
+                <div>Loading...</div>
+            </template>
+        </Suspense>
+        <Welcome />
         <div style="height: 10000px"></div>
         <NuxtImg
-            src="image/deblured-cutty-fox.jpg"
+            src="/image/deblured-cutty-fox.jpg"
             width="600"
             height="400"
             loading="lazy"
         />
     </div>
 </template>
+<script setup lang="ts">
+const Welcome = defineAsyncComponent(() => import("~/components/Welcome.vue"));
+</script>
