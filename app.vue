db745b4ee8e6a1fd60dc0502351d0444e06772f2
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-27 15:36:08 +0700

    b2

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <div style="height: 10000px"></div>
+        <NuxtImg
+            src="image/deblured-cutty-fox.jpg"
+            width="600"
+            height="400"
+            loading="lazy"
+        />
+    </div>
 </template>
