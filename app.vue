745cb3f99a88e5c1262978581c8a84781f29874b
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-27 14:11:09 +0700

    b1

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <Suspense>
+        <component :is="MyComponent" />
+    </Suspense>
+    <Suspense>
+        <component :is="MyComponent1" v-if="false" />
+    </Suspense>
+    <MyComponent2 v-if="false"/>
 </template>
+
+<script setup>
+const MyComponent = defineAsyncComponent(() =>
+    import("@/components/MyComponent.vue")
+);
+const MyComponent1 = defineAsyncComponent(() =>
+    import("@/components/MyComponent1.vue")
+);
+
+</script>
---
+++ components/MyComponent.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="">
+        <h1>This is my component</h1>
+    </div>
+</template>
+
+<style lang="css" scoped></style>
